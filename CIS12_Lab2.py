6c8470f5d531805f33daae31fb865ecb8e445071
Conrad Salazar <themetroid7998@gmail.com>
2024-09-02 23:39:48 -0700

    Initial Commit, more work to be done.

---
+++ CIS12_Lab2.py
@@ -0,0 +1,8 @@
+Name = "Conrad Salazar"
+Age = 19
+Height = 6
+FavColor = "black"
+print ("My name is", Name, ".")
+print ("I am", Age, "years old.")
+print ("I am", Height, "feet tall, sort of.")
+print ("My favorite color is", FavColor, "because I am such an original person.")
